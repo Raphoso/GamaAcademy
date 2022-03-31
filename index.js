5c7fd116c6c7a65ec8440b838d46cdbbe790b149
Raphoso <raphaelgamapr@gmail.com>
2022-03-31 11:27:14 -0300

    Commit aula

--- index.js
+++ index.js
@@ -2,11 +2,46 @@
 //     alert("mensagem apresentada no carregamento do sistema");
 // }
 
+function populaTabela() {
+    if (localStorage.getItem("produtos")) {
+        let produtos = JSON.parse(localStorage.getItem("produtos"))
+
+        $("#tblProdutos tbody").html("")
+        produtos.forEach((produto) => {
+            $("#tblProdutos tbody").append(`<tr>
+                <td>${produto.nome}</td>
+                <td>${produto.quantidade}</td>
+                <td>${produto.valor}</td> 
+                <td style="width: 50px"><button type="button" class= "btn btn-info">E</button></td>
+                <td style="width: 50px"><button type="button" class= "btn btn-danger">D</button></td>
+            </tr>`)
+        })
+    }
+}
+
 $(() => {
     // Código executado no carregamento da página
-    // alert("mensagem apresentada no carregamento do sistema");
+        populaTabela()
 
-    $("#btnSalvar").click(() => {
-        console.log("FUNÇÃO CLICK FUNCIONOU")
+        $("#btnSalvar").click(() => {
+        
+        let produto =  {}
+        produto.nome = $("#nmProduto").val()
+        produto.quantidade = $("#quantidade").val()
+        produto.valor = $("#vlrProduto").val()
+
+        let listaProdutos = []
+        listaProdutos.push(produto)
+
+        localStorage.setItem("produtos" , JSON.stringify(listaProdutos))
+
+        alert("Produto salvo com sucesso")
+
+        $("#nmProduto").val("")
+        $("#quantidade").val("")
+        $("#vlrProduto").val("")
+
+        populaTabela()
+
     })
 })
